e4f74eab2a268637af60f856b437632687a89cb5
Breno Darós <brenosouzadaros@gmail.com>
2023-12-12 16:20:44 -0300

    Projeto default.

---
+++ src/main/java/com/bsd/betatts/TtsElevenlabsApplication.java
@@ -0,0 +1,13 @@
+package com.bsd.betatts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtsElevenlabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtsElevenlabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsd/betatts/TtsElevenlabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsd.betatts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtsElevenlabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
